3778fcb2aa9522c8b5db69e7714baf7e01e01b8e
Deyan Dobromirov <dvd_video@abv.bg>
2018-08-30 22:35:41 +0300

    Fixed: A bunch of run time errors in the advanced phys-prop

--- lua/weapons/gmod_tool/stools/physprop_adv.lua
+++ lua/weapons/gmod_tool/stools/physprop_adv.lua
@@ -1,10 +1,13 @@
 local gsTool = "physprop_adv"
 local gsLisp = "physicsmaterialsadv_"
 local gclBgn = Color(0, 0, 0, 210)
-local gclTxt = Color(0, 0, 0, 0)
+local gclTxt = Color(0, 0, 0, 255)
 local gclBox = Color(250, 250, 200, 255)
-
-local gsFont = "Trebuchet18"
+local gsInvm = "N/A"
+local gnRadm = (20*0.618)
+local gnRadr = (gnRadm-gnRadm%2)
+local gsFont = "Trebuchet24"
+local gnTacn = TEXT_ALIGN_CENTER 
 
 TOOL.Category = "Construction"
 TOOL.Name = "Physical Properties Adv"
@@ -14,9 +17,10 @@ TOOL.ClientConVar = {
   [ "material_type"  ] = 1,
   [ "material_name"  ] = 1,
   [ "material_draw"  ] = 1,
-  
+  [ "material_cash"  ] = 1
 }
 
+table.Empty(list.GetForEdit(gsLisp.."type"))
 list.Add(gsLisp.."type", "special"      )
 list.Add(gsLisp.."type", "concrete"     )
 list.Add(gsLisp.."type", "metal"        )
@@ -28,6 +32,7 @@ list.Add(gsLisp.."type", "miscellaneous")
 list.Add(gsLisp.."type", "organic"      )
 list.Add(gsLisp.."type", "manufactured" )
 
+table.Empty(list.GetForEdit(gsLisp.."special"))
 list.Add(gsLisp.."special",  "default"            )
 list.Add(gsLisp.."special",  "default_silent"     )
 list.Add(gsLisp.."special",  "floatingstandable"  )
@@ -37,12 +42,14 @@ list.Add(gsLisp.."special",  "no_decal"           )
 list.Add(gsLisp.."special",  "player"             )
 list.Add(gsLisp.."special",  "player_control_clip")
 
+table.Empty(list.GetForEdit(gsLisp.."concrete"))
 list.Add(gsLisp.."concrete", "brick"         )
 list.Add(gsLisp.."concrete", "concrete"      )
 list.Add(gsLisp.."concrete", "concrete_block")
 list.Add(gsLisp.."concrete", "gravel"        )
 list.Add(gsLisp.."concrete", "rock"          )
 
+table.Empty(list.GetForEdit(gsLisp.."metal"))
 list.Add(gsLisp.."metal","canister"             )
 list.Add(gsLisp.."metal","chain"                )
 list.Add(gsLisp.."metal","chainlink"            )
@@ -68,6 +75,7 @@ list.Add(gsLisp.."metal","solidmetal"           )
 list.Add(gsLisp.."metal","strider"              )
 list.Add(gsLisp.."metal","weapon"               )
 
+table.Empty(list.GetForEdit(gsLisp.."wood"))
 list.Add(gsLisp.."wood", "wood"          )
 list.Add(gsLisp.."wood", "Wood_Box"      )
 list.Add(gsLisp.."wood", "Wood_Furniture")
@@ -75,6 +83,7 @@ list.Add(gsLisp.."wood", "Wood_Plank"    )
 list.Add(gsLisp.."wood", "Wood_Panel"    )
 list.Add(gsLisp.."wood", "Wood_Solid"    )
 
+table.Empty(list.GetForEdit(gsLisp.."terrain"))
 list.Add(gsLisp.."terrain", "dirt"         )
 list.Add(gsLisp.."terrain", "grass"        )
 list.Add(gsLisp.."terrain", "gravel"       )
@@ -84,19 +93,23 @@ list.Add(gsLisp.."terrain", "sand"         )
 list.Add(gsLisp.."terrain", "slipperyslime")
 list.Add(gsLisp.."terrain", "antlionsand"  )
 
+table.Empty(list.GetForEdit(gsLisp.."liquid"))
 list.Add(gsLisp.."liquid", "slime")
 list.Add(gsLisp.."liquid", "water")
 list.Add(gsLisp.."liquid", "wade" )
 
+table.Empty(list.GetForEdit(gsLisp.."frozen"))
 list.Add(gsLisp.."frozen", "snow"    )
 list.Add(gsLisp.."frozen", "ice"     )
 list.Add(gsLisp.."frozen", "gmod_ice")
 
+table.Empty(list.GetForEdit(gsLisp.."miscellaneous"))
 list.Add(gsLisp.."miscellaneous", "carpet"      )
 list.Add(gsLisp.."miscellaneous", "ceiling_tile")
 list.Add(gsLisp.."miscellaneous", "computer"    )
 list.Add(gsLisp.."miscellaneous", "pottery"     )
 
+table.Empty(list.GetForEdit(gsLisp.."organic"))
 list.Add(gsLisp.."organic", "alienflesh" )
 list.Add(gsLisp.."organic", "antlion"    )
 list.Add(gsLisp.."organic", "armorflesh" )
@@ -106,6 +119,7 @@ list.Add(gsLisp.."organic", "foliage"    )
 list.Add(gsLisp.."organic", "watermelon" )
 list.Add(gsLisp.."organic", "zombieflesh")
 
+table.Empty(list.GetForEdit(gsLisp.."manufactured"))
 list.Add(gsLisp.."manufactured", "jeeptire"               )
 list.Add(gsLisp.."manufactured", "jalopytire"             )
 list.Add(gsLisp.."manufactured", "rubber"                 )
@@ -128,9 +142,19 @@ list.Add(gsLisp.."manufactured", "glassbottle"            )
 list.Add(gsLisp.."manufactured", "combine_glass"          )
 
 if(CLIENT) then
-  TOOL.Information = { { name = "left" } }
+  TOOL.Information = {
+    { name = "info", stage = 1},
+    { name = "left"  },
+    { name = "left_use", icon2 = "gui/e.png"},
+    { name = "right" },
+    { name = "reload"}
+  }
   language.Add("tool."..gsTool..".name"              , "Physics Properties Adv")
   language.Add("tool."..gsTool..".desc"              , "Advanced and extended version of the original physics properties tool")
+  language.Add("tool."..gsTool..".left"              , "Apply the selected physical property")
+  language.Add("tool."..gsTool..".right"             , "Cache the selected physical property")
+  language.Add("tool."..gsTool..".reload"            , "Reset original physical property")
+  language.Add("tool."..gsTool..".left_use"          , "Apply cached physical property")
   language.Add("tool."..gsTool..".material_type"     , "Select material type from the ones listed here")
   language.Add("tool."..gsTool..".material_type_def" , "Select type...")
   language.Add("tool."..gsTool..".material_name"     , "Select material name from the ones listed here")
@@ -143,13 +167,17 @@ end
 
 function TOOL:NotifyPlayer(sText, sType, vRet)
   if(SERVER) then -- Send notification to client that something happened
-    local oPly = self:GetOwner()
-    pPly:SendLua("GAMEMODE:AddNotify(\""..sText.."\", NOTIFY_"..sType..", 6)")
-    pPly:SendLua("surface.PlaySound(\"ambient/water/drip"..math.random(1, 4)..".wav\")")
+    local mePly = self:GetOwner()
+    mePly:SendLua("GAMEMODE:AddNotify(\""..sText.."\", NOTIFY_"..sType..", 6)")
+    mePly:SendLua("surface.PlaySound(\"ambient/water/drip"..math.random(1, 4)..".wav\")")
   end; return vRet
 end
 
-function TOOL:GetMaterialInfo(vT, vN) -- Avoid returning a copy by list-get to make it faster
+local function getConvar(sN)
+  return GetConVar(gsTool.."_"..sN)
+end
+
+local function getMaterialInfo(vT, vN) -- Avoid returning a copy by list-get to make it faster
   local tT = list.GetForEdit(gsLisp.."type") -- No edit though just read it
   local iT = math.Clamp(tonumber(vT or 1), 1, #tT)
   local tN = list.GetForEdit(gsLisp..tT[iT]) -- No edit though same here
@@ -161,43 +189,71 @@ function TOOL:GetMaterialDraw()
   return ((self:GetClientNumber("material_draw") or 0) ~= 0)
 end
 
+function TOOL:GetMaterialCash()
+  return tostring(self:GetClientInfo("material_cash") or gsInvm)
+end
+
+function TOOL:CheckButton(iIn)
+  local cmdPress = self:GetOwner():GetCurrentCommand()
+  return (bit.band(cmdPress:GetButtons(),iIn) ~= 0)
+end
+
 function TOOL:LeftClick(tr)
+  if(not (tr and tr.Hit)) then return false end
   local trEnt, trBon = tr.Entity, tr.PhysicsBone
   if(not (trEnt and trEnt:IsValid())) then return false end
   if(trEnt:IsPlayer() or trEnt:IsWorld()) then return false end
-
   -- Make sure there's a physics object to manipulate
   if(SERVER and not util.IsValidPhysicsObject(trEnt, trBon)) then return false end
-
   -- Client can bail out here and assume we're going ahead
   if(CLIENT) then return true end
-
   -- Get client's CVars
-  local owner   = self:GetOwner()
+  local mePly, matprop = self:GetOwner()
   local gravity = (self:GetClientNumber("gravity_toggle") == 1)
-  local matprop = self:GetMaterialInfo(self:GetClientNumber("material_type"), self:GetClientNumber("material_name"))
-  if(matprop:len() == 0) then return self:NotifyPlayer("Empty material", "ERROR", false) end
-  
-  -- Set the properties
-  construct.SetPhysProp(owner, trEnt, trBon, nil, {GravityToggle = gravity, Material = matprop})
-
+  if(self:CheckButton(IN_USE)) then matprop = self:GetMaterialCash()
+  else matprop = getMaterialInfo(self:GetClientNumber("material_type"),
+                                 self:GetClientNumber("material_name")) end
+  if(matprop:len() == 0 or matprop == gsInvm) then
+    return self:NotifyPlayer("Material apply invalid: "..matprop, "ERROR", false) end
+  construct.SetPhysProp(mePly, trEnt, trBon, nil, {GravityToggle = gravity, Material = matprop})
   DoPropSpawnedEffect(trEnt)
+  return self:NotifyPlayer("Material apply: "..matprop, "GENERIC", true)
+end
 
-  return true
+function TOOL:RightClick(tr)
+  if(not (tr and tr.Hit)) then return false end
+  local mePly, trPro = self:GetOwner(), tr.SurfaceProps
+  local matprop = (trPro and util.GetSurfacePropName(trPro) or gsInvm)
+  if(matprop:len() == 0 or matprop == gsInvm) then
+    return self:NotifyPlayer("Material cache invalid: "..matprop, "ERROR", false) end
+  mePly:ConCommand(gsTool.."_material_cash "..matprop)
+  return self:NotifyPlayer("Material copy: "..matprop, "UNDO", true)
+end
+
+function TOOL:Reload(tr)
+  if(not (tr and tr.Hit)) then return false end
+  local trEnt, trBon = tr.Entity, tr.PhysicsBone
+  local mePly, trPro = self:GetOwner(), tr.SurfaceProps
+  local matprop = (trPro and util.GetSurfacePropName(trPro) or gsInvm)
+  if(matprop:len() == 0 or matprop == gsInvm) then
+    return self:NotifyPlayer("Material reset invalid: "..matprop, "ERROR", false) end
+  if(SERVER and not util.IsValidPhysicsObject(trEnt, trBon)) then
+    return self:NotifyPlayer("Material reset physics invalid: "..matprop, "ERROR", false) end
+  construct.SetPhysProp(mePly, trEnt, trBon, nil, {Material = matprop})
+  return self:NotifyPlayer("Material reset: "..matprop, "GENERIC", true)
 end
 
 function TOOL:DrawHUD(w, h)
   if(not self:GetMaterialDraw()) then return end
-  local oPly = LocalPlayer()
-  local oTr  = oPly:GetEyeTrace()
-  local trEnt, nR, nP = oTr.Entity, 8, oTr.SurfaceProps
+  local oTr = LocalPlayer():GetEyeTrace()
+  local trEnt, nP = oTr.Entity, oTr.SurfaceProps
   if(not (trEnt and trEnt:IsValid())) then return end
-  local x, y = trEnt:LocalToWorld(trEnt:OBBCenter()):ToScreen()
-  local matprop = (nP and util.GetSurfacePropName(nP) or "N/A"); surface.SetFont(gsFont)
+  local xyP = trEnt:LocalToWorld(trEnt:OBBCenter()):ToScreen()
+  local matprop = (nP and util.GetSurfacePropName(nP) or gsInvm); surface.SetFont(gsFont)
   local tw, th = surface.GetTextSize(matprop)
-  draw.RoundedBox(nR, x - tw/2 - 12, y - th/2 - 4, tw + 24, th + 8, gclBgn)
-  draw.RoundedBox(nR, x - tw/2 - 10, y - th/2 - 2, tw + 20, th + 4, gclBox)
-  draw.SimpleText(matprop, gsFont, x, y, gclTxt, 1, 1)
+  draw.RoundedBox(gnRadr, xyP.x - tw/2 - 12, xyP.y - th/2 - 4, tw + 24, th + 8, gclBgn)
+  draw.RoundedBox(gnRadr, xyP.x - tw/2 - 10, xyP.y - th/2 - 2, tw + 20, th + 4, gclBox)
+  draw.SimpleText(matprop, gsFont, xyP.x, xyP.y, gclTxt, gnTacn, gnTacn)
 end
 
 local ConVarsDefault = TOOL:BuildConVarList()
@@ -213,7 +269,8 @@ function TOOL.BuildCPanel( CPanel )
     Options    = {["Default"] = ConVarsDefault},
     CVars      = table.GetKeys(ConVarsDefault)
   }); nY = pItem:GetTall() + 2
-
+  local matprop = getMaterialInfo(getConvar("material_type"):GetInt(),
+                                  getConvar("material_name"):GetInt())
     -- http://wiki.garrysmod.com/page/Category:DComboBox
   local tT = list.GetForEdit(gsLisp.."type")
   local pComboType = vgui.Create("DComboBox", CPanel)
@@ -225,29 +282,29 @@ function TOOL.BuildCPanel( CPanel )
         for iT = 1, #tT do pComboType:AddChoice(tT[iT], iT) end
   nY = nY + pComboType:GetTall() + 2
     -- http://wiki.garrysmod.com/page/Category:DComboBox
-  local tN = list.GetForEdit(gsLisp.."type")
   local pComboName = vgui.Create("DComboBox", CPanel)
         pComboName:SetPos(2, nY)
         pComboName:SetSortItems(false)
         pComboName:SetTall(20)
         pComboName:SetTooltip(language.GetPhrase("tool."..gsTool..".material_name"))
-        pComboName:SetValue(language.GetPhrase("tool."..gsTool..".material_name_def"))
-        pComboType.OnSelect = function(pnSelf, nInd, sVal, anyData)
-          RunConsoleCommand(gsTool.."_material_type", anyData)
-          local iT = math.Clamp(anyData, 1, #tT)
-          local tN = list.GetForEdit(gsLisp..tT[iT])
-          pComboName:Clear()
-          pComboName:SetValue(language.GetPhrase("tool."..gsTool..".material_name_def"))
-          for iN = 1, #tN do pComboName:AddChoice(tN[iN], iN) end
-          pComboName.OnSelect = function(pnSelf, nInd, sVal, anyData)
-            RunConsoleCommand(gsTool.."_material_name", anyData)
-          end; iNam = iNam + 1
-        end
+        pComboName:SetValue(language.GetPhrase("tool."..gsTool..".material_name_def").." "..matprop)
+  -- Material list selection
+  pComboType.OnSelect = function(pnSelf, nInd, sVal, anyData)
+    RunConsoleCommand(gsTool.."_material_type", anyData)
+    local iT = math.Clamp(anyData, 1, #tT)
+    local tN = list.GetForEdit(gsLisp..tT[iT])
+    pComboName:Clear()
+    pComboName:SetValue(language.GetPhrase("tool."..gsTool..".material_name_def"))
+    for iN = 1, #tN do pComboName:AddChoice(tN[iN], iN) end
+    pComboName.OnSelect = function(pnSelf, nInd, sVal, anyData)
+      RunConsoleCommand(gsTool.."_material_name", anyData)
+    end
+  end
   CPanel:AddItem(pComboType)
   CPanel:AddItem(pComboName)
 
-  pItem = CPanel:CheckBox (language.GetPhrase("tool."..gsTool..".gravity_toggle_con"), gsToolPrefL.."gravity_toggle")
+  pItem = CPanel:CheckBox (language.GetPhrase("tool."..gsTool..".gravity_toggle_con"), gsTool.."_gravity_toggle")
           pItem:SetTooltip(language.GetPhrase("tool."..gsTool..".gravity_toggle"))
-  pItem = CPanel:CheckBox (language.GetPhrase("tool."..gsTool..".material_draw_con"), gsToolPrefL.."material_draw")
+  pItem = CPanel:CheckBox (language.GetPhrase("tool."..gsTool..".material_draw_con"), gsTool.."_material_draw")
           pItem:SetTooltip(language.GetPhrase("tool."..gsTool..".material_draw"))
 end
